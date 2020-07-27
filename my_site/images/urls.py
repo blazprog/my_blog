367b64a9601189351977976b133a32f0cc4e582b
Blaz <Bla Koroec>
2020-07-27 18:23:25 +0200

    next and previous

--- my_site/images/urls.py
+++ my_site/images/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.image_list, name='image_list'),
     path('image/<int:pk>/', views.image_detail, name='image_detail'),
+    path('previous/<int:pk>/', views.image_previous, name='image_previous'),
+    path('next/<int:pk>/', views.image_next, name='image_next'),
 ]
--- my_site/images/views.py
+++ my_site/images/views.py
@@ -13,3 +13,23 @@ def image_list(request):
 def image_detail(request, pk):
     image = get_object_or_404(Image, pk=pk)
     return render(request, 'images/image.html', {"image": image})
+
+
+def image_next(request, pk):
+    next_image = Image.objects.filter(id__gt=pk)
+    if next_image:
+        next_image = next_image[0]
+    else:
+        next_image = get_object_or_404(Image, pk=pk)
+
+    return render(request, 'images/image.html', {"image": next_image})
+
+
+def image_previous(request, pk):
+    previous_image = Image.objects.filter(id__lt=pk)
+    if previous_image:
+        previous_image = previous_image[len(previous_image)-1]
+    else:
+        previous_image = get_object_or_404(Image, pk=pk)
+
+    return render(request, 'images/image.html', {"image": previous_image})
